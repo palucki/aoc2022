4800a1e6451e33ca8bdcd09a91132adda3b4ccf2
palucki <piotrek.palucki@gmail.com>
2023-10-22 16:31:32 +0200

    Day 8 2/2

--- day8.cpp
+++ day8.cpp
@@ -95,6 +95,82 @@ int main()
 
     std::cout << "Total visible " << total_visible << '\n';
 
+    std::cout << "Calculating scenic score...\n";
+
+    auto in_map = [&map](int i, int j) -> bool {
+        return i >= 0 && i < map.size() && j >= 0 && j < map[0].size();
+    };
+
+    int best_scenic_score = 0;
+    for(int i = 0; i < map.size(); ++i)
+    {
+        for(int j = 0; j < map[i].size(); ++j)
+        {
+            auto tree = map[i][j];
+            
+            //check top
+            int top_distance = 0;
+            for(int k = i - 1; k >= 0; --k)
+            {
+                if(in_map(k, j))
+                {
+                    top_distance++;
+                    if(map[k][j] >= tree)
+                        break;
+                }
+            }
+
+            //check bottom
+            int bottom_distance = 0;
+            for(int k = i + 1; k < map.size(); ++k)
+            {
+                if(in_map(k, j))
+                {
+                    bottom_distance++;  
+                    if(map[k][j] >= tree)
+                        break;
+                } 
+            }
+            
+            //check left
+            int left_distance = 0;
+            for(int k = j - 1; k >= 0; --k)
+            {
+                if(in_map(i, k))
+                {
+                    left_distance++;
+                    if(map[i][k] >= tree)
+                        break;
+                }
+            }
+
+            //check right
+            int right_distance = 0;
+            for(int k = j + 1; k < map[i].size(); ++k)
+            {
+                if(in_map(i, k))
+                {
+                    right_distance++;
+                    if(map[i][k] >= tree)
+                        break;
+                }
+            }
+
+            auto tree_scenic_score = top_distance * bottom_distance * left_distance * right_distance;
+            
+            std::cout << i << "," << j << ": (" << top_distance << "," << left_distance << "," << bottom_distance << "," << right_distance << ")" 
+                      << " score " << tree_scenic_score << " tree " << tree << "\n";
+            
+            if(tree_scenic_score > best_scenic_score)
+            {
+                best_scenic_score = tree_scenic_score;
+                std::cout << "New best scenic score at " << i << "," << j << " is " << best_scenic_score << '\n';
+            }
+        }
+    }
+
+    std::cout << "Best scenic score " << best_scenic_score << '\n';
+
     in.close();
     return 0;
 }
