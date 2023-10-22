0dcc272e64059a942bc9e8f11eb6d8126fc5f8ef
palucki <piotrek.palucki@gmail.com>
2023-10-22 15:24:29 +0200

    Day 8 1/2

---
+++ day8.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <list>
+#include <stack>
+
+int main()
+{
+    std::fstream in("day8_input.txt", std::ios::in);
+    if(!in.is_open())
+    {
+        std::cout << "Could not open file for reading!\n";
+        return -1;
+    }
+
+    std::vector<std::vector<unsigned char>> map;
+    std::vector<std::vector<bool>> visible;
+    std::string line;
+    
+    while(std::getline(in, line))
+    {
+        std::cout << "Input " << line << '\n';
+        map.push_back(std::vector<unsigned char>(line.begin(), line.end()));
+        visible.push_back(std::vector<bool>(line.size()));
+    }
+
+    std::vector<int> highest_from_top(map[0].size(), -1);
+    std::vector<int> highest_from_bottom(map[0].size(), -1);
+    std::vector<int> highest_from_left(map.size(), -1);
+    std::vector<int> highest_from_right(map.size(), -1);
+
+    for(int i = 0; i < map.size(); ++i)
+    {
+        for(int j = 0; j < map[i].size(); ++j)
+        {
+            // std::cout << i << "," << j << "\n";
+            auto tree = map[i][j];
+            if(tree > highest_from_left[i])
+            {
+                highest_from_left[i] = tree;
+                visible[i][j] = true;
+            }
+           
+            if(tree > highest_from_top[j])
+            {
+                highest_from_top[j] = tree;
+                visible[i][j] = true;
+            }
+        }
+    }
+
+    for(int i = map.size() - 1; i >= 0; --i)
+    {
+        for(int j = map[i].size() - 1; j >= 0; --j)
+        {
+            // std::cout << i << " " << j << "\n";
+            auto tree = map[i][j];
+            if(tree > highest_from_right[i])
+            {
+                highest_from_right[i] = tree;
+                visible[i][j] = true;
+            }
+
+            if(tree > highest_from_bottom[j])
+            {
+                highest_from_bottom[j] = tree;
+                visible[i][j] = true;
+            }
+        }
+    }
+           
+    int total_visible = 0;
+    for(int i = 0; i < map.size(); ++i)
+    {
+        for(int j = 0; j < map[i].size(); ++j)
+        {
+            if(visible[i][j])
+            {
+                std::cout << map[i][j];
+                total_visible++;    
+            }
+            else
+            {
+                std::cout << 'x';
+            }
+        }
+        std::cout << '\n';
+    }
+    // std::cout << i << "," << j << " = " << tree << (visible ? " " : " not ") << "visible\n";
+
+
+    std::cout << "Total visible " << total_visible << '\n';
+
+    in.close();
+    return 0;
+}
