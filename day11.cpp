ed5cb3d99c17d248579dfe5cc3f71c74227a83da
palucki <piotrek.palucki@gmail.com>
2023-11-03 14:34:27 +0100

    Completed day 11 2/2

--- day11.cpp
+++ day11.cpp
@@ -25,7 +25,9 @@ struct Monkey
 
             times_inspected++;
             
-            item = item / 3;
+            // item = item / 3;
+            // magic trick: modulo by multiplication of all test values
+            item = item % 9699690;
 
             // std::cout << "item after decreasing " << item << '\n';
 
@@ -42,7 +44,7 @@ struct Monkey
         }
     }
 
-    void receive(int item)
+    void receive(unsigned long long item)
     {
         items.push_back(item);
     }
@@ -58,10 +60,10 @@ struct Monkey
     }
 
     int times_inspected = 0;
-    std::deque<int> items;
+    std::deque<unsigned long long> items;
 
-    std::function<int(int)> func;
-    std::function<bool(int)> test;
+    std::function<unsigned long long(unsigned long long)> func;
+    std::function<bool(unsigned long long)> test;
     Monkey* true_receiver{nullptr};
     Monkey* false_receiver{nullptr};
 };
@@ -78,50 +80,50 @@ int main()
     Monkey m7;
 
     m0.items = {54, 89, 94};
-    m0.func = [](const int val) { return val * 7; };
-    m0.test = [](const int val) { return val % 17 == 0; };
+    m0.func = [](const unsigned long long val) { return val * 7; };
+    m0.test = [](const unsigned long long val) { return val % 17 == 0; };
     m0.true_receiver = &m5;
     m0.false_receiver = &m3;
     
     m1.items = {66, 71};
-    m1.func = [](const int val) { return val + 4; };
-    m1.test = [](const int val) { return val % 3 == 0; };
+    m1.func = [](const unsigned long long val) { return val + 4; };
+    m1.test = [](const unsigned long long val) { return val % 3 == 0; };
     m1.true_receiver = &m0;
     m1.false_receiver = &m3;
     
     m2.items = {76, 55, 80, 55, 55, 96, 78};
-    m2.func = [](const int val) { return val + 2; };
-    m2.test = [](const int val) { return val % 5 == 0; };
+    m2.func = [](const unsigned long long val) { return val + 2; };
+    m2.test = [](const unsigned long long val) { return val % 5 == 0; };
     m2.true_receiver = &m7;
     m2.false_receiver = &m4;
     
     m3.items = {93, 69, 76, 66, 89, 54, 59, 94};
-    m3.func = [](const int val) { return val + 7; };
-    m3.test = [](const int val) { return val % 7 == 0; };
+    m3.func = [](const unsigned long long val) { return val + 7; };
+    m3.test = [](const unsigned long long val) { return val % 7 == 0; };
     m3.true_receiver = &m5;
     m3.false_receiver = &m2;
 
     m4.items = {80, 54, 58, 75, 99};
-    m4.func = [](const int val) { return val * 17; };
-    m4.test = [](const int val) { return val % 11 == 0; };
+    m4.func = [](const unsigned long long val) { return val * 17; };
+    m4.test = [](const unsigned long long val) { return val % 11 == 0; };
     m4.true_receiver = &m1;
     m4.false_receiver = &m6;
 
     m5.items = {69, 70, 85, 83};
-    m5.func = [](const int val) { return val + 8; };
-    m5.test = [](const int val) { return val % 19 == 0; };
+    m5.func = [](const unsigned long long val) { return val + 8; };
+    m5.test = [](const unsigned long long val) { return val % 19 == 0; };
     m5.true_receiver = &m2;
     m5.false_receiver = &m7;
 
     m6.items = {89};
-    m6.func = [](const int val) { return val + 6; };
-    m6.test = [](const int val) { return val % 2 == 0; };
+    m6.func = [](const unsigned long long val) { return val + 6; };
+    m6.test = [](const unsigned long long val) { return val % 2 == 0; };
     m6.true_receiver = &m0;
     m6.false_receiver = &m1;
 
     m7.items = {62, 80, 58, 57, 93, 56};
-    m7.func = [](const int val) { return val * val; };
-    m7.test = [](const int val) { return val % 13 == 0; };
+    m7.func = [](const unsigned long long val) { return val * val; };
+    m7.test = [](const unsigned long long val) { return val % 13 == 0; };
     m7.true_receiver = &m6;
     m7.false_receiver = &m4;
 
@@ -135,7 +137,7 @@ int main()
     std::cout << "6: "; m6.print_items();
     std::cout << "7: "; m7.print_items();
 
-    for(int i = 0; i < 20; ++i)
+    for(int i = 0; i < 10000 ; ++i)
     {
         m0.operate();
         m1.operate();
