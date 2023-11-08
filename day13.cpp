2bb01d1bef9c04a3e8a02e7b8eda4f599038f817
palucki <piotrek.palucki@gmail.com>
2023-11-08 14:06:16 +0100

    Completed day 13 2/2

--- day13.cpp
+++ day13.cpp
@@ -62,7 +62,7 @@ int compare(Node& lhs, Node& rhs)
 
     if(lhs.dataNode())
     {
-        lhs.children.push_back(Node{lhs.data});
+        lhs.children.emplace_back(Node{lhs.data});
         lhs.data = -1;
 
         return compare(lhs, rhs);
@@ -70,7 +70,7 @@ int compare(Node& lhs, Node& rhs)
 
     if(rhs.dataNode())
     {
-        rhs.children.push_back(Node{rhs.data});
+        rhs.children.emplace_back(Node{rhs.data});
         rhs.data = -1;
 
         return compare(lhs, rhs);
@@ -150,12 +150,15 @@ int main()
     std::string line1, line2;
     int pair_index = 1;
     int result = 0;
+
+    std::vector<Node> nodes;
     
     while(std::getline(in, line1) && std::getline(in, line2))
     {
         // std::cout << "1: " << line1 << '\n';
         Node head1;
         auto idx = parse(head1, 0, -1, line1);
+        nodes.emplace_back(head1);
         // std::cout << "P: ";
         // head1.print();
         // std::cout << '\n';
@@ -163,15 +166,16 @@ int main()
         // std::cout << "2: " << line2 << '\n';
         Node head2;
         idx = parse(head2, 0, -1, line2);
+        nodes.emplace_back(head2);
         // std::cout << "P: ";
         // head2.print();
         // std::cout << '\n';
         
-        if(compare(head1, head2) == -1)
-        {
-            std::cout << "pair index " << pair_index << '\n';
-            result += pair_index;
-        }
+        // if(compare(head1, head2) == -1)
+        // {
+        //     std::cout << "pair index " << pair_index << '\n';
+        //     result += pair_index;
+        // }
         
         pair_index++;
         // consume empty line
@@ -180,93 +184,32 @@ int main()
 
     in.close();
 
-    std::cout << "Result " << result << '\n';
+    Node n2{2};
+    Node n6{6};
+    nodes.push_back(n2);
+    nodes.push_back(n6);
 
-    // {
-    //     // [1,1,3,1,1] vs [1,1,5,1,1]
-    //     Node left;
-    //     left.children.push_back(Node{1});
-    //     left.children.push_back(Node{1});
-    //     left.children.push_back(Node{3});
-    //     left.children.push_back(Node{1});
-    //     left.children.push_back(Node{1});
-    //     left.print();
-    //     std::cout << '\n';
+    std::sort(nodes.begin(), nodes.end(), [](Node& lhs, Node& rhs)
+    {
+        if(compare(lhs, rhs) == -1)
+            return true;
 
-    //     Node right;
-    //     right.children.push_back(Node{1});
-    //     right.children.push_back(Node{1});
-    //     right.children.push_back(Node{3});
-    //     right.children.push_back(Node{1});
-    //     right.children.push_back(Node{1});
-    //     right.print();
-    //     std::cout << '\n';
-    //     std::cout << compare(left, right) << '\n';
-    // }
+        return false;
+    });
 
-    // {
-    //     // [] vs [3]
-    //     Node left;
-    //     left.print();
-    //     std::cout << '\n';
+    auto i = 1;
+    for(auto& n : nodes)
+    {
+        if(compare(n, n2) == 0 || compare(n, n6) == 0)
+        {
+            std::cout << i << " : ";
+            n.print();
+            std::cout << '\n';
+        }
+        ++i;
+    }
 
-    //     Node right;
-    //     right.children.push_back(Node{3});
-    //     right.print();
-    //     std::cout << '\n';
-    //     std::cout << compare(left, right) << '\n';
-    // }
 
-    // {
-    //     //[9] vs [[8,7,6]]
-    //     Node left;
-    //     left.children.push_back(Node{9}); 
-    //     left.print();
-    //     std::cout << '\n';
-
-    //     Node right;
-    //     Node subtree;
-    //     subtree.children.push_back(Node{8});
-    //     subtree.children.push_back(Node{7});
-    //     subtree.children.push_back(Node{6});
-    //     right.children.push_back(subtree);
-    //     right.print();
-    //     std::cout << '\n';
-    //     std::cout << compare(left, right) << '\n';
-    // }
-
-    // {
-    //     //[9] vs [[8,7,6]]
-    //     Node left;
-    //     left.children.push_back(Node{9}); 
-    //     left.print();
-    //     std::cout << '\n';
-
-    //     Node right;
-    //     Node subtree;
-    //     subtree.children.push_back(Node{8});
-    //     subtree.children.push_back(Node{7});
-    //     subtree.children.push_back(Node{6});
-    //     right.children.push_back(subtree);
-    //     right.print();
-    //     std::cout << '\n';
-    //     std::cout << compare(left, right) << '\n';
-    // }
-
-    // {
-    //     //[[[]]] vs [[]]
-    //     Node left;
-    //     left.children.push_back(Node{});
-    //     left.children[0].children.push_back(Node{});
-    //     left.print(); 
-    //     std::cout << '\n';
-
-    //     Node right;
-    //     right.children.push_back(Node{});
-    //     right.print(); 
-    //     std::cout << '\n';
-    //     std::cout << compare(left, right) << '\n';
-    // }
 
     return 0;
 }
