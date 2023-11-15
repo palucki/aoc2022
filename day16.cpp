80d97c656c317a2fd22eeb087a647dde935a30bc
palucki <piotrek.palucki@gmail.com>
2023-11-15 15:14:05 +0100

    Day 16 - WIP for greedy approach (not enough)

---
+++ day16.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+// trim from start
+static inline std::string &ltrim(std::string &s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         std::not1(std::ptr_fun<int, int>(std::isspace)))
+                .base(),
+            s.end());
+    return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s)
+{
+    return ltrim(rtrim(s));
+}
+
+std::vector<std::string> split(std::string s, std::string delimiter)
+{
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos)
+    {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
+struct Valve
+{
+    std::string id;
+    int flow_rate;
+    std::vector<Valve *> children;
+};
+
+int pressure(Valve *v, int time_left, std::map<std::pair<std::string, int>, int>& memo, std::set<std::string> opened_valves = {})
+{
+    std::cout << "id " << v->id << " time left " << time_left << " memo size " << memo.size() << '\n';
+
+    // if(memo.find({v->id, time_left}) != memo.end())
+    // {
+        // std::cout << "Returning memo value: " << memo[{v->id, time_left}] << "\n";
+    // }
+    // if(memo.find({v->id, time_left}) != memo.end())
+    // {
+    //     std::cout << "Value in memo " << memo[{v->id, time_left}] << '\n';
+    //     return memo[{v->id, time_left}];
+    // }
+
+    if (time_left <= 1)
+    {
+        // memo[{v->id, time_left}] = 0;
+        return 0;
+    }
+
+    std::vector<int> results;
+    for (auto *vc : v->children)
+    {
+        const auto c_pressure = pressure(vc, time_left - 1, memo, opened_valves);
+        results.push_back(c_pressure);
+        
+        // if(memo.find({vc->id, time_left - 1}) != memo.end())
+        // {
+        //     memo[{vc->id, time_left - 1}] = std::max(c_pressure, memo[{vc->id, time_left - 1}]);
+        // }
+    }
+
+    if (opened_valves.find(v->id) == opened_valves.end())
+    {
+        // std::cout << "opening " << v->id << " with flow rate " << v->flow_rate << '\n';
+        opened_valves.insert(v->id);
+        auto open_valve_result = ((time_left - 1) * v->flow_rate) + pressure(v, time_left - 1, memo, opened_valves);
+        results.push_back(open_valve_result);
+
+        // if(memo.find({v->id, time_left - 1}) != memo.end())
+        // {
+        //     memo[{v->id, time_left - 1}] = std::max(open_valve_result, memo[{v->id, time_left - 1}]);
+        // }
+    }
+
+    if (results.empty())
+    {
+        // memo[{v->id, time_left}] = 0;
+        return 0;
+    }
+
+    auto result = *std::max_element(results.begin(), results.end());
+    return result;
+    // memo[{v->id, time_left}] = std::max(result, memo[{v->id, time_left}]);
+    // return memo[{v->id, time_left}];
+
+    // auto final_result = std::max(result, memo[{v->id, time_left}]);
+
+    // std::cout << "Result " << v->id << " / " << time_left <<  " is " <<  memo[{v->id, time_left}] << '\n';
+
+}
+
+int main()
+{
+    // std::fstream in("day16_input.txt", std::ios::in);
+    // if (!in.is_open())
+    // {
+    //     std::cout << "Could not open file for reading!\n";
+    //     return -1;
+    // }
+
+    // std::map<std::string, Valve *> valves;
+
+    // std::string line;
+    // while (std::getline(in, line))
+    // {
+    //     std::cout << line << '\n';
+
+    //     std::stringstream ss{line};
+    //     std::string tmp, valve, flow_rate, outputs;
+
+    //     ss >> tmp >> valve >> tmp >> tmp >> flow_rate;
+    //     auto res = split(flow_rate, "=");
+    //     flow_rate = res[1].substr(0, res[1].size() - 1);
+
+    //     auto single_valve_pos = line.find("leads to valve ");
+    //     if (single_valve_pos == std::string::npos)
+    //     {
+    //         auto multiple_valves_pos = line.find("lead to valves ");
+    //         if (multiple_valves_pos == std::string::npos)
+    //         {
+    //             return -1;
+    //         }
+
+    //         outputs = line.substr(multiple_valves_pos + 15);
+    //     }
+    //     else
+    //     {
+    //         outputs = line.substr(single_valve_pos + 15);
+    //     }
+
+    //     auto out_valves = split(outputs, ",");
+    //     std::cout << valve << " " << flow_rate << "\n";
+
+    //     if (valves.find(valve) != valves.end())
+    //     {
+    //         auto *v = valves[valve];
+    //         v->flow_rate = std::stoi(flow_rate);
+    //         for (auto o : out_valves)
+    //         {
+    //             std::cout << "->" << trim(o) << '\n';
+    //         }
+    //     }
+    //     else 
+    //     {
+    //         Valve *new_v = new Valve{valve, std::stoi(flow_rate)};
+
+    //     }
+    // }
+
+    Valve aa{"AA", 0};
+    Valve bb{"BB", 13};
+    Valve cc{"CC", 2};
+    Valve dd{"DD", 20};
+    Valve ee{"EE", 3};
+    Valve ff{"FF", 0};
+    Valve gg{"GG", 0};
+    Valve hh{"HH", 22};
+    Valve ii{"II", 0};
+    Valve jj{"JJ", 21};
+
+    // aa.children = {&dd, &bb};
+    // bb.children = {&cc, &aa};
+    // cc.children = {&dd, &bb};
+    // dd.children = {&cc, &aa};
+    aa.children = {&dd, &ii, &bb};
+    bb.children = {&cc, &aa};
+    cc.children = {&dd, &bb};
+    dd.children = {&cc, &aa, &ee};
+    ee.children = {&ff, &dd};
+    ff.children = {&ee, &gg};
+    gg.children = {&ff, &hh};
+    hh.children = {&gg};
+    ii.children = {&aa, &jj};
+    jj.children = {&ii};
+
+    std::map<std::pair<std::string, int>, int> memo;
+    auto res = pressure(&aa, 6, memo);
+
+    std::cout << "max pressure: " << res << '\n';
+
+    return 0;
+}
