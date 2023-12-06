6f5265f2e1e42db016ad737cfc489af5066e44a5
palucki <piotrek.palucki@gmail.com>
2023-12-06 22:49:41 +0100

    Completed day 17 1/2

--- day17.cpp
+++ day17.cpp
@@ -25,23 +25,28 @@ struct Shape
 {
     bool move(char direction, Map& map)
     {
+        for(const auto& a : occupied)
+        {
+            map[a.second][a.first] = '.';
+        }
+
         std::vector<Pos> new_pos = occupied;
         
         if(direction == '>')
         {
-            // std::cout << "move right\n";
+            std::cout << "move right\n";
             for(auto& e : new_pos)
                 e.first += 1;
         }
         else if(direction == '<')
         {
-            // std::cout << "move left\n";
+            std::cout << "move left\n";
             for(auto& e : new_pos)
                 e.first -= 1;
         }
         else 
         {
-            // std::cout << "move down\n";
+            std::cout << "move down\n";
             for(auto& e : new_pos)
                 e.second -= 1;
         }
@@ -50,6 +55,13 @@ struct Shape
         {
             if(a.first < 0 || a.first >= WIDTH || a.second < 0 || map[a.second][a.first] == '#')
             {
+                std::cout << " new coords: " << a.first << " " << a.second << '\n';
+
+                for(const auto& a : occupied)
+                {
+                    map[a.second][a.first] = '#';
+                }
+
                 return false;
             }
         }
@@ -63,7 +75,7 @@ struct Shape
         
         for(const auto& a : occupied)
         {
-            map[a.second][a.first] = '#';
+            map[a.second][a.first] = '@';
         }
 
         return true;
@@ -80,10 +92,18 @@ struct Shape
         for(auto& e : occupied)
         {
             e.second += height;
-            map[e.second][e.first] = '#';
+            map[e.second][e.first] = '@';
         }
 
         return true;
+    }
+
+    int top()
+    {
+        return std::max_element(occupied.begin(), occupied.end(), [](const Pos& lhs, const Pos& rhs)
+        {
+            return lhs.second < rhs.second;
+        })->second + 1;
     }
 
     std::vector<Pos> occupied;
@@ -122,7 +142,7 @@ int main()
     }
 
     std::string sequence;
-    int rock_number = 1;
+    int rock_number = 0;
 
     Map map;
     init(map);
@@ -132,39 +152,58 @@ int main()
         return -1;
     }
 
-    bool stop = false;
     Shape line{{{2, 0}, {3, 0}, {4, 0}, {5, 0}}};
-    // Shape line{{{2, 0}, {3, 0}, {4, 0}, {5, 0}}};
+    Shape cross{{{2, 1}, {3, 1}, {4, 1}, {3, 0}, {3, 2}}};
+    Shape l{{{2, 0}, {3, 0}, {4, 0}, {4, 1}, {4, 2}}};
+    Shape stick{{{2, 0}, {2, 1}, {2, 2}, {2, 3}}};
+    Shape cube{{{2, 0}, {2, 1}, {3, 0}, {3, 1}}};
+    std::vector<Shape> shapes{line, cross, l, stick, cube};
 
     int height_so_far = 0;
-    if(!line.place(map, height_so_far + 3))
-    {
-        return -1;
-    }
-
     int move_number = 0;
-    print(map, 10);
 
     while(rock_number < 2022)
+    // while(rock_number < 11)
     {
+        bool stop = false;
+
+        auto rock = shapes[rock_number % shapes.size()];
+
+        rock.place(map, height_so_far + 3);
+        std::cout << "Placing shape at " << height_so_far + 3 << '\n';
+        print(map, 20);
         // const auto rock = rocks[rock_number % rocks.size()];
 
         while(!stop)
         {
-            line.move(sequence[move_number++ % sequence.size()], map);
-
-            if(!line.move('V', map))
+            const auto move = sequence[move_number++ % sequence.size()];
+            if(!rock.move(move, map))
             {
-                std::cout << "Can not move down " << move_number-1 << "\n";
+                std::cout << "Cannot move " << move << '\n';
+            }
+
+            if(!rock.move('V', map))
+            {
+                std::cout << "Can not move down after " << move_number-1 << " moves\n";
                 stop = true;
             }
+            print(map, 20);
         }
 
+        std::cout << "rock top at: " << rock.top() << '\n';
+        
+        for(const auto& e : rock.occupied)
+        {
+            std::cout << e.second << "," << e.first << " ";
+        }
+        std::cout << '\n';
+
+        height_so_far = std::max(height_so_far, rock.top());
         rock_number++;
-        // std::cout << "After rock " << rock_number << '\n';
+        std::cout << "After rock " << rock_number << " max height " << height_so_far << '\n';
     }
 
-    print(map, 10);
+    // print(map, 10);
 
     return 0;
 }
